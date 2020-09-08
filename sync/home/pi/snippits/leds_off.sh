1d6ce7121eb0f97b94f6b75b82e59e7fc3b766ca
Nathan Fain <cyphunk@gmail.com>
2020-09-08 14:33:12 +0200

    maybe I was wrong about that

--- sync/home/pi/snippits/leds_off.sh
+++ sync/home/pi/snippits/leds_off.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # wait 120 seconds and then turn off
 echo "### leds_off.sh (every 120 seconds)"
-# actually it appears somewhere else some other process may re-enable so lets put in a loop
-# lord have mercy on us
-while [ 1 ]; do
 sleep 120
 echo 0 > /sys/class/leds/led1/brightness
 echo 0 > /sys/class/leds/led0/brightness
-done
